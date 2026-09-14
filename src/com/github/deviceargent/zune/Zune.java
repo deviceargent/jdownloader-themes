3b79375a4985ce40d38ddc311b257fb59affcf3c
deviceargent <deviceargent@users.noreply.github.com>
2026-09-14 04:16:29 -0300

    Revert "experiment: paint Zune main tab content"
    This reverts commit acc8a7841fb7b4662c1aae80a66f8e332cf2f0c7.

--- src/com/github/deviceargent/zune/Zune.java
+++ src/com/github/deviceargent/zune/Zune.java
@@ -1,15 +1,6 @@
 package com.github.deviceargent.zune;
 
 import com.formdev.flatlaf.FlatDarkLaf;
-import java.awt.AWTEvent;
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Toolkit;
-import java.awt.Window;
-import java.awt.event.AWTEventListener;
-import java.awt.event.WindowEvent;
-import javax.swing.JTabbedPane;
-import javax.swing.JComponent;
 import javax.swing.UIDefaults;
 
 public class Zune
@@ -19,32 +10,6 @@ public class Zune
 
 	public static boolean setup() {
 		return setup( new Zune() );
-	}
-
-	@Override
-	public void initialize() {
-		super.initialize();
-		AWTEventListener listener = event -> {
-			if( event instanceof WindowEvent && event.getID() == WindowEvent.WINDOW_OPENED )
-				applyTabbedPaneUI( (Window) ((WindowEvent) event).getWindow() );
-		};
-		Toolkit.getDefaultToolkit().addAWTEventListener( listener, AWTEvent.WINDOW_EVENT_MASK );
-		for( Window window : Window.getWindows() )
-			applyTabbedPaneUI( window );
-	}
-
-	private static void applyTabbedPaneUI( Component component ) {
-		if( component instanceof JTabbedPane && component.getClass().getName().equals( "jd.gui.swing.jdgui.MainTabbedPane" ) ) {
-			JTabbedPane tabs = (JTabbedPane) component;
-			tabs.setUI( new ZuneTabbedPaneUI() );
-			tabs.setOpaque( false );
-			Component selected = tabs.getSelectedComponent();
-			if( selected instanceof JComponent )
-				((JComponent) selected).setOpaque( false );
-		}
-		if( component instanceof Container )
-			for( Component child : ((Container) component).getComponents() )
-				applyTabbedPaneUI( child );
 	}
 
 	public static void installLafInfo() {
